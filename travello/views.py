72c19a10cf038497891bab0ee125a8e95b2e846c
Sagar Gatla <sgatla@tibco.com>
2020-05-11 11:56:44 +0530

    dynamic website

--- travello/views.py
+++ travello/views.py
@@ -6,27 +6,27 @@ from .models import Desti
 
 def index(request):
 
-    dest1=Desti()
-    dest1.name='Mumbai'
-    dest1.desc='The city that never sleeps'
-    dest1.img='destination_4.jpg'
-    dest1.price=1000
-    dest1.offer=True
+    #dest1=Desti()
+    #dest1.name='Mumbai'
+    #dest1.desc='The city that never sleeps'
+    #dest1.img='destination_4.jpg'
+    #dest1.price=1000
+    #dest1.offer=True
 
-    dest2=Desti()
-    dest2.name='Hyderabad'
-    dest2.desc='First Biryani second sherwani'
-    dest2.img='destination_5.jpg'
-    dest2.price=1000
-    dest2.offer=False
+    #dest2=Desti()
+    #dest2.name='Hyderabad'
+    #dest2.desc='First Biryani second sherwani'
+    #dest2.img='destination_5.jpg'
+    #dest2.price=1000
+    #dest2.offer=False
 
-    dest3=Desti()
-    dest3.name='Delhi'
-    dest3.desc='The capital of india'
-    dest3.img='destination_6.jpg'
-    dest3.price=1000
-    dest3.offer=True
+    #dest3=Desti()
+    #dest3.name='Delhi'
+    #dest3.desc='The capital of india'
+    #dest3.img='destination_6.jpg'
+    #dest3.price=1000
+    #dest3.offer=True
 
-    dests=[dest1, dest2, dest3]
-
+    #dests=[dest1, dest2, dest3] 
+    dests = Desti.objects.all()
     return render(request,'index.html',{'dests': dests})
